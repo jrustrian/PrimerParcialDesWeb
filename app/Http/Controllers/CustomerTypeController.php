33935d0f6938b7263a99fe82da6b29d79aaf1673
Jonathan Rustrian <jonathanmiusic34@gmail.com>
2022-08-06 12:02:55 -0600

    se crea modelo, controlador y migracion

---
+++ app/Http/Controllers/CustomerTypeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\customer_type;
+use Illuminate\Http\Request;
+
+class CustomerTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\customer_type  $customer_type
+     * @return \Illuminate\Http\Response
+     */
+    public function show(customer_type $customer_type)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\customer_type  $customer_type
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(customer_type $customer_type)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\customer_type  $customer_type
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, customer_type $customer_type)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\customer_type  $customer_type
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(customer_type $customer_type)
+    {
+        //
+    }
+}
